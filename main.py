02388752430c57a86e1dcf3971900b9c8627e774
Kritika <24kritikasharma24@gmail.com>
2021-06-22 18:13:59 +0530

    basic root widow setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter 
+
+#defining window
+root = tkinter.Tk()
+root.title('Tick Things')
+root.iconbitmap('check.ico')
+root.geometry('400x400')
+root.resizable(0,0)
+
+
+# run the main window loop
+root.mainloop()
