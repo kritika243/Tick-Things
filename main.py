f26c37e9ea8ae3b45743a73b165205afd5f65ee8
Kritika <24kritikasharma24@gmail.com>
2021-06-23 21:53:05 +0530

    output frame layout done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import tkinter 
+import tkinter
+from tkinter import font 
 
 #defining window
 root = tkinter.Tk()
 root.title('Tick Things')
 root.iconbitmap('check.ico')
-root.geometry('400x400')
+root.geometry('450x400')
 root.resizable(0,0)
 
 # defining fonts and colors
@@ -33,8 +34,19 @@ list_entry.grid(row=0, column=0)
 list_add_button.grid(row=0, column=1)
 
 # output frame layout
-my_list_box = tkinter.Listbox(output_frame, height=15, width=40, borderwidth=3, font=my_root)
+my_list_box = tkinter.Listbox(output_frame, height=13, width=40, borderwidth=3, font=my_root)
 my_list_box.grid(row=0, column=0)
+
+# Button frame layout
+list_remove_button=tkinter.Button(button_frame, text='Remove Thing', borderwidth=2, font=my_root, bg=button_color)
+list_clear_button = tkinter.Button(button_frame, text='Clear All', borderwidth=2, font=my_root, bg=button_color)
+save_button = tkinter.Button(button_frame, text='Save Things', borderwidth=2, font=my_root, bg=button_color)
+quit_button = tkinter.Button(button_frame, text='Quit', borderwidth=2, font=my_root, bg=button_color, command=root.destroy)
+
+list_remove_button.grid(row=0, column=0)
+list_clear_button.grid(row=0, column=1)
+save_button.grid(row=0, column=2)
+quit_button.grid(row=0, column=3)
 
 
 # run the main window loop
